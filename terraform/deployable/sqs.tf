f913aaef28f1cab29faa43356826b4f9d988b212
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-10-31 11:04:17 +0000

    Update readme with run instructions. Correct terraform to validate and plan based on hard coded region providers.

--- terraform/deployable/sqs.tf
+++ terraform/deployable/sqs.tf
@@ -18,18 +18,18 @@ module "sqs__ApproximateAgeOfOldestMessge" {
 }
 */
 
-resource "aws_cloudwatch_metric_alarm" "cloudwatch_metric_alarm" {
+resource "aws_cloudwatch_metric_alarm" "euw1_cloudwatch_sqs_approx_age" {
   # iterate over count to setup multiple alarms
-  count 		            = length(var.sqs__ApproximateAgeOfOldestMessage)
-  provider                  = "aws.${var.sqs__ApproximateAgeOfOldestMessage[count.index].Region}"
-  alarm_name                = "${var.sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName}_alarm"
+  count 		            = length(var.eu-west-1__sqs__ApproximateAgeOfOldestMessage)
+  provider                  = "aws.eu-west-1"
+  alarm_name                = "${var.eu-west-1__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName}_alarm"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 2
   #metric_name               = "ApproximateAgeOfOldestMessage"
   #namespace                 = "AWS/SQS"
   #period                    = 300
   #statistic                 = "Maximum"
-  threshold                 = var.sqs__ApproximateAgeOfOldestMessage[count.index].Threshold
+  threshold                 = var.eu-west-1__sqs__ApproximateAgeOfOldestMessage[count.index].Threshold
   alarm_description         = "Check that messages are being processed in a reasonable time frame."
   #alarm_actions		        = [var.alarm_actions]
   #dimensions		        = local.sqs_dimensions
@@ -53,8 +53,51 @@ resource "aws_cloudwatch_metric_alarm" "cloudwatch_metric_alarm" {
       stat        = "Maximum"
 
       dimensions = {
-	    QueueName = var.sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName
+	    QueueName = var.eu-west-1__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName
       }
     }
    }
 }
+
+
+resource "aws_cloudwatch_metric_alarm" "euw2_cloudwatch_sqs_approx_age" {
+  # iterate over count to setup multiple alarms
+  count 		            = length(var.eu-west-2__sqs__ApproximateAgeOfOldestMessage)
+  provider                  = "aws.eu-west-2"
+  alarm_name                = "${var.eu-west-2__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName}_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  #metric_name               = "ApproximateAgeOfOldestMessage"
+  #namespace                 = "AWS/SQS"
+  #period                    = 300
+  #statistic                 = "Maximum"
+  threshold                 = var.eu-west-2__sqs__ApproximateAgeOfOldestMessage[count.index].Threshold
+  alarm_description         = "Check that messages are being processed in a reasonable time frame."
+  #alarm_actions		        = [var.alarm_actions]
+  #dimensions		        = local.sqs_dimensions
+  #ok_actions                = var.ok_actions
+  #insufficient_data_actions  = var.insufficient_data_actions
+  #datapoints_to_alarm       = var.datapoints_to_alarm
+  #unit			     = var.unit
+  #extended_statistic        = var.extended_statistic
+  #treat_missing_data        = var.treat_missing_data
+  #evaluate_low_sample_count_percentiles = var.evaluate_low_sample_count_percentiles
+  #metric_query		     = var.metric_query
+  #tags                      = local.sqs_alarm_tags
+
+  metric_query {
+    id = "m1"
+
+    metric {
+      metric_name = "ApproximateAgeOfOldestMessage"
+      namespace   = "AWS/SQS"
+      period      = 300
+      stat        = "Maximum"
+
+      dimensions = {
+	    QueueName = var.eu-west-2__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName
+      }
+    }
+   }
+}
+
--- terraform/deployable/variables.tf
+++ terraform/deployable/variables.tf
@@ -1,4 +1,10 @@
-variable "sqs__ApproximateAgeOfOldestMessage" {
+variable "eu-west-1__sqs__ApproximateAgeOfOldestMessage" {
+  description = "List of metrics derived from aws cloudwatch list-metrics"
+  type        = "list"
+  default     = []
+}
+
+variable "eu-west-2__sqs__ApproximateAgeOfOldestMessage" {
   description = "List of metrics derived from aws cloudwatch list-metrics"
   type        = "list"
   default     = []
