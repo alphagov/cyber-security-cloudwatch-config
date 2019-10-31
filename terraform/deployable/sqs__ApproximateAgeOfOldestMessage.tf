b32b5ecef799a5cffad4b2e6bf25e63422e9cbce
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-10-31 11:27:31 +0000

    Rename and simplify cloudwatch alarm config for sqs and upate readme.

---
+++ terraform/deployable/sqs__ApproximateAgeOfOldestMessage.tf
@@ -0,0 +1,38 @@
+
+resource "aws_cloudwatch_metric_alarm" "euw1_cloudwatch_sqs_approx_age" {
+  # iterate over count to setup multiple alarms
+  count 		            = length(var.eu-west-1__sqs__ApproximateAgeOfOldestMessage)
+  provider                  = "aws.eu-west-1"
+  alarm_name                = "${var.eu-west-1__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName}_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  threshold                 = var.eu-west-1__sqs__ApproximateAgeOfOldestMessage[count.index].Threshold
+  alarm_description         = "Check that messages are being processed in a reasonable time frame."
+  metric_name               = "ApproximateAgeOfOldestMessage"
+  namespace                 = "AWS/SQS"
+  period                    = 300
+  statistic                 = "Maximum"
+  dimensions		        = {
+    QueueName = var.eu-west-1__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "euw2_cloudwatch_sqs_approx_age" {
+  # iterate over count to setup multiple alarms
+  count 		            = length(var.eu-west-2__sqs__ApproximateAgeOfOldestMessage)
+  provider                  = "aws.eu-west-2"
+  alarm_name                = "${var.eu-west-2__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName}_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  threshold                 = var.eu-west-2__sqs__ApproximateAgeOfOldestMessage[count.index].Threshold
+  alarm_description         = "Check that messages are being processed in a reasonable time frame."
+  metric_name               = "ApproximateAgeOfOldestMessage"
+  namespace                 = "AWS/SQS"
+  period                    = 300
+  statistic                 = "Maximum"
+  dimensions		        = {
+    QueueName = var.eu-west-2__sqs__ApproximateAgeOfOldestMessage[count.index].ResourceName
+  }
+}
+
