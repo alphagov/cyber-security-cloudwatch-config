1eb919aa1f3b03e7f441f69a068a463c9f3b252f
Aaron Hine <aaron.hine@digital.cabinet-office.gov.uk>
2019-12-03 16:37:19 +0000

    Added TF files this time

---
+++ terraform/deployable/kinesis__PutRecordsSuccess.tf
@@ -0,0 +1,51 @@
+variable "eu-west-1__kinesis__PutRecordsSuccess" {
+  description = "List of metrics derived from aws cloudwatch list-metrics"
+  type        = list
+  default     = []
+}
+
+variable "eu-west-2__kinesis__PutRecordsSuccess" {
+  description = "List of metrics derived from aws cloudwatch list-metrics"
+  type        = list
+  default     = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "euw1_cloudwatch_kinesis_put_records_success" {
+  # iterate over count to setup multiple alarms
+  count               = length(var.eu-west-1__kinesis__PutRecordsSuccess)
+  provider            = aws.eu-west-1
+  alarm_name          = "${var.eu-west-1__kinesis__PutRecordsSuccess[count.index].ResourceName}_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  threshold           = var.eu-west-1__kinesis__PutRecordsSuccess[count.index].Threshold
+  alarm_description   = "Tracks the read position across all shards and consumers in the stream. If an iterator's age passes 50% of the retention period (by default, 24 hours, configurable up to 7 days), there is risk for data loss due to record expiration."
+  metric_name         = "PutRecordsSuccess"
+  namespace           = "AWS/Kinesis"
+  period              = 300
+  statistic           = "Maximum"
+  dimensions = {
+    QueueName = var.eu-west-1__kinesis_PutRecordsSuccess[count.index].ResourceName
+  }
+  alarm_actions       = ["${local.euw1_sns_health_topic}"]
+  ok_actions          = ["${local.euw1_sns_health_topic}"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "euw2_cloudwatch_kinesis_put_records_success" {
+  # iterate over count to setup multiple alarms
+  count               = length(var.eu-west-2__kinesis__PutRecordsSuccess)
+  provider            = aws.eu-west-2
+  alarm_name          = "${var.eu-west-2__kinesis__PutRecordsSuccess[count.index].ResourceName}_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  threshold           = var.eu-west-2__kinesis__PutRecordsSuccess[count.index].Threshold
+  alarm_description   = "Tracks the read position across all shards and consumers in the stream. If an iterator's age passes 50% of the retention period (by default, 24 hours, configurable up to 7 days), there is risk for data loss due to record expiration."
+  metric_name         = "PutRecordsSuccess"
+  namespace           = "AWS/Kinesis"
+  period              = 300
+  statistic           = "Maximum"
+  dimensions = {
+    QueueName = var.eu-west-2__kinesis_PutRecordsSuccess[count.index].ResourceName
+  }
+  alarm_actions       = ["${local.euw2_sns_health_topic}"]
+  ok_actions          = ["${local.euw2_sns_health_topic}"]
+}
---
+++ terraform/deployable/sqs__NumberOfMessagesSent.tf
@@ -0,0 +1,93 @@
+variable "eu-west-1__sqs__NumberOfMessagesSent" {
+  description = "List of metrics derived from aws cloudwatch list-metrics"
+  type        = list
+  default     = []
+}
+
+variable "eu-west-2__sqs__NumberOfMessagesSent" {
+  description = "List of metrics derived from aws cloudwatch list-metrics"
+  type        = list
+  default     = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "euw1_cloudwatch_sqs_num_of_messages_sent_high" {
+  # iterate over count to setup multiple alarms
+  count               = length(var.eu-west-1__sqs__NumberOfMessagesSent)
+  provider            = aws.eu-west-1
+  alarm_name          = "${var.eu-west-1__sqs__NumberOfMessagesSent[count.index].ResourceName}_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  threshold           = var.eu-west-1__sqs__NumberOfMessagesSent[count.index].Threshold
+  alarm_description   = "Check the number of messages added to the queue is not too high."
+  metric_name         = "NumberOfMessagesSent"
+  namespace           = "AWS/SQS"
+  period              = 300
+  statistic           = "Maximum"
+  dimensions = {
+    QueueName = var.eu-west-1__sqs__NumberOfMessagesSent[count.index].ResourceName
+  }
+  alarm_actions       = ["${local.euw1_sns_health_topic}"]
+  ok_actions          = ["${local.euw1_sns_health_topic}"]
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "euw2_cloudwatch_sqs_num_of_messages_sent_high" {
+  # iterate over count to setup multiple alarms
+  count               = length(var.eu-west-2__sqs__NumberOfMessagesSent)
+  provider            = aws.eu-west-2
+  alarm_name          = "${var.eu-west-2__sqs__NumberOfMessagesSent[count.index].ResourceName}_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  threshold           = var.eu-west-2__sqs__NumberOfMessagesSent[count.index].Threshold
+  alarm_description   = "Check the number of messages added to the queue is not too high."
+  metric_name         = "NumberOfMessagesSent"
+  namespace           = "AWS/SQS"
+  period              = 300
+  statistic           = "Maximum"
+  dimensions = {
+    QueueName = var.eu-west-2__sqs__NumberOfMessagesSent[count.index].ResourceName
+  }
+  alarm_actions       = ["${local.euw2_sns_health_topic}"]
+  ok_actions          = ["${local.euw2_sns_health_topic}"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "euw1_cloudwatch_sqs_num_of_messages_sent_low" {
+  # iterate over count to setup multiple alarms
+  count               = length(var.eu-west-1__sqs__NumberOfMessagesSent)
+  provider            = aws.eu-west-1
+  alarm_name          = "${var.eu-west-1__sqs__NumberOfMessagesSent[count.index].ResourceName}_alarm"
+  comparison_operator = "LowerThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  threshold           = var.eu-west-1__sqs__NumberOfMessagesSent[count.index].Threshold
+  alarm_description   = "Check the number of messages added to the queue is not too low."
+  metric_name         = "NumberOfMessagesSent"
+  namespace           = "AWS/SQS"
+  period              = 300
+  statistic           = "Maximum"
+  dimensions = {
+    QueueName = var.eu-west-1__sqs__NumberOfMessagesSent[count.index].ResourceName
+  }
+  alarm_actions       = ["${local.euw1_sns_health_topic}"]
+  ok_actions          = ["${local.euw1_sns_health_topic}"]
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "euw2_cloudwatch_sqs_num_of_messages_sent_low" {
+  # iterate over count to setup multiple alarms
+  count               = length(var.eu-west-2__sqs__NumberOfMessagesSent)
+  provider            = aws.eu-west-2
+  alarm_name          = "${var.eu-west-2__sqs__NumberOfMessagesSent[count.index].ResourceName}_alarm"
+  comparison_operator = "LowerThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  threshold           = var.eu-west-2__sqs__NumberOfMessagesSent[count.index].Threshold
+  alarm_description   = "Check the number of messages added to the queue is not too low."
+  metric_name         = "NumberOfMessagesSent"
+  namespace           = "AWS/SQS"
+  period              = 300
+  statistic           = "Maximum"
+  dimensions = {
+    QueueName = var.eu-west-2__sqs__NumberOfMessagesSent[count.index].ResourceName
+  }
+  alarm_actions       = ["${local.euw2_sns_health_topic}"]
+  ok_actions          = ["${local.euw2_sns_health_topic}"]
+}
