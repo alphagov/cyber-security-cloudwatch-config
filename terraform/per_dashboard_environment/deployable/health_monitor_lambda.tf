8432fe7a4a7a8eaff3a4bce3f574d31e4f68942a
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-12-06 13:41:32 +0000

    And SNS topic ARNS to health monitor lambda environment. Correct lambda invoke permission name.

--- terraform/per_dashboard_environment/deployable/health_monitor_lambda.tf
+++ terraform/per_dashboard_environment/deployable/health_monitor_lambda.tf
@@ -1,5 +1,8 @@
 locals {
-  zipfile = "../../../lambda/health_package/health_package.zip"
+  zipfile           = "../../../lambda/health_package/health_package.zip"
+  slack_sns_arn     = element(aws_sns_topic.euw2_sns_topics.*.arn, index(var.sns_topic_names, "notify_slack_lambda"))
+  pagerduty_sns_arn = element(aws_sns_topic.euw2_sns_topics.*.arn, index(var.sns_topic_names, "notify_pagerduty_lambda"))
+  dashboard_sns_arn = element(aws_sns_topic.euw2_sns_topics.*.arn, index(var.sns_topic_names, "notify_dashboard_lambda"))
 }
 
 resource "aws_lambda_function" "health_monitor_lambda" {
@@ -10,5 +13,14 @@ resource "aws_lambda_function" "health_monitor_lambda" {
   handler          = "lambda_handler.health_monitor_handler"
   timeout          = 60
   runtime          = "python3.7"
+
+  environment {
+    variables = {
+      LOGLEVEL          = "DEBUG"
+      PAGERDUTY_SNS_ARN = local.pagerduty_sns_arn
+      SLACK_SNS_ARN     = local.slack_sns_arn
+      DASHBOARD_SNS_ARN = local.dashboard_sns_arn
+    }
+  }
 }
 
