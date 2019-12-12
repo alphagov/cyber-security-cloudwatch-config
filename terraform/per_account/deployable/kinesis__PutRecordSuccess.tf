395d46f622e7c7346ee4d3a5095cae16e1d47848
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-12-12 15:16:30 +0000

    Correct comparison operator

--- terraform/per_account/deployable/kinesis__PutRecordSuccess.tf
+++ terraform/per_account/deployable/kinesis__PutRecordSuccess.tf
@@ -15,7 +15,7 @@ resource "aws_cloudwatch_metric_alarm" "euw1_cloudwatch_kinesis_put_record_succe
   count               = length(var.eu-west-1__kinesis__PutRecordSuccess)
   provider            = aws.eu-west-1
   alarm_name          = "${var.eu-west-1__kinesis__PutRecordSuccess[count.index].ResourceName}_alarm"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
+  comparison_operator = "LessThanThreshold"
   evaluation_periods  = 2
   threshold           = var.eu-west-1__kinesis__PutRecordSuccess[count.index].Threshold
   alarm_description   = "Tracks the read position across all shards and consumers in the stream. If an iterator's age passes 50% of the retention period (by default, 24 hours, configurable up to 7 days), there is risk for data loss due to record expiration."
