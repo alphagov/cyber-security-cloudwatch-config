0a38fc20fe3369422ed7b3889d60080626f7e82a
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-01-10 16:15:01 +0000

    Create a daily concourse pipeline to test that the slack hooks configured in SSM all work.

--- terraform/per_dashboard_environment/deployable/reference.tf
+++ terraform/per_dashboard_environment/deployable/reference.tf
@@ -1,3 +1,3 @@
 module "reference_accounts" {
-  source = "git::ssh://git@github.com/alphagov/cyber-security-terraform//reference/accounts?ref=05518f0"
+  source = "git::ssh://git@github.com/alphagov/cyber-security-terraform//reference/accounts?ref=c1b7f64"
 }
