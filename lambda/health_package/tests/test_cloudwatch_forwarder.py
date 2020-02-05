e2fe3e1a76d32ff31afde1ff62218c4b1c65ca5f
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-02-05 14:27:26 +0000

    Add failing unit test to test pipeline run order.

--- lambda/health_package/tests/test_cloudwatch_forwarder.py
+++ lambda/health_package/tests/test_cloudwatch_forwarder.py
@@ -102,3 +102,7 @@ def test_send_to_health_monitor(mock_sqs_send_message_response):
     with stubber:
         response = send_to_health_monitor(event)
         assert response == mock_sqs_send_message_response
+
+
+def test_fail():
+    assert False
