65e881c15d5c29ad5f493642d15c20b2dfa0389f
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-09-28 09:45:19 +0100

    Correct path to pipeline terraform

--- terraform/deployments/670214072732_pipeline/module-codebuild-self-update.tf
+++ terraform/deployments/670214072732_pipeline/module-codebuild-self-update.tf
@@ -3,7 +3,7 @@ module "codebuild_self_update" {
   codebuild_service_role_name = data.aws_iam_role.pipeline_role.name
   deployment_account_id       = data.aws_caller_identity.current.account_id
   deployment_role_name        = "CodePipelineDeployerRole_${data.aws_caller_identity.current.account_id}"
-  terraform_directory         = "terraform/deployments/${data.aws_caller_identity.current.account_id}"
+  terraform_directory         = "terraform/deployments/${data.aws_caller_identity.current.account_id}_pipeline"
   codebuild_image             = var.default_container_image
   pipeline_name               = var.service_name
   stage_name                  = "Pipeline"
